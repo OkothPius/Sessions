8e6bc09ac6d5213ab974de8e350b99b3fcaaa4ec
OkothPius <orukopius8@gmail.com>
2023-05-21 04:39:21 +0300

    feat: update session

--- index.js
+++ index.js
@@ -4,19 +4,20 @@ const session = require('express-session');
 
 const app = express();
 const port = 3001;
+require('dotenv').config();
 
 // Enable cookie parsing
 app.use(cookieParser());
 
 // Set up session middleware
 app.use(session({
-  secret: '38sisjsk92',
+  secret: process.env.SESSION_SECRET,
   resave: false,
   saveUninitialized: true,
   cookie: {
     maxAge: 3600000, // Expires after 1 hour (in milliseconds)
     httpOnly: true, // Accessible only via HTTP(S)
-    secure: false, // Sent only over HTTPS
+    secure: false, // Sent only over HTTPS (if used when true in localhost it does not allow login. Ensure it is false when testing the application)
   },   
 }));
 
@@ -54,10 +55,22 @@ app.post('/login', (req, res) => {
 });
 
 
+// app.get('/logout', (req, res) => {
+//   req.session.destroy();
+//   res.redirect('/');
+// });
+
 app.get('/logout', (req, res) => {
-  req.session.destroy();
-  res.redirect('/');
+  req.session.destroy(err => {
+    if (err) {
+      console.log(err)
+    } else {
+      res.clearCookie('connect.sid');
+      res.redirect('/');
+    }
+  });
 });
+
 
 // Start the server
 app.listen(port, () => {
